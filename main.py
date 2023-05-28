3daa8f1c0f201bd9149e8dc68be919f15e48c0cd
dane <dhcjensen@gmail.com>
2023-05-27 21:53:02 -0700

    Added json ser/deser for the task and registry classes.

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import json
 from display import HEADER
-from ledger import Ledger, LedgerEncoder
-# from tasks import Task
+from registry import Registry, RegistryEncoder, RegistryDecoder
+from tasks import Task
+import shutil
 
 
 def main():
-    ledger = Ledger()
+    registry = Registry()
+    x, y = shutil.get_terminal_size()
     keys = ('title', 'importance', 'duration', 'desc')
     args1 = ('dishes', None, None, 'do them')
     args2 = ('vaccuum', 5, 15, 'do it')
-    # task1 = Task(*args1)
-    # print(task1)
+    args3 = ('garbage', 2, 10, 'gross')
 
     d1 = dict(zip(keys, args1))
     d2 = dict(zip(keys, args2))
+    d3 = dict(zip(keys, args3))
 
-    ledger.add_task(**d1)
-    ledger.add_task(**d2)
-    padding = '\n' + ' ' * 20
+    registry.add_task(Task(**d1))
+    registry.add_task(Task(**d2))
+    padding = '\n' + ' ' * (y - 14)
     print(padding.join(s for s in HEADER))
 
-    print(ledger)
-    print(json.dumps(ledger, cls=LedgerEncoder, indent=2))
+    j_registry = json.dumps(registry, cls=RegistryEncoder, indent=2)
+    with open('./data/data.json', 'w') as f:
+        f.write(j_registry)
+
+    registry = json.loads(j_registry, cls=RegistryDecoder)
+
+    registry.add_task(Task(**d3))
+
+    j_registry = json.dumps(registry, cls=RegistryEncoder, indent=2)
+    # print(j_registry)
 
 
 if __name__ == "__main__":
